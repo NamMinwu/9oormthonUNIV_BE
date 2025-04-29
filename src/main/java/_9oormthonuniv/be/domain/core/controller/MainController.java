626b9e84e9bfec6e753b234c842be22885d16760
NamMinwu <minwoo000625@naver.com>
2025-04-29 15:24:56 +0900

    [CHORE] : jenkins cd test4

--- src/main/java/_9oormthonuniv/be/domain/core/controller/MainController.java
+++ src/main/java/_9oormthonuniv/be/domain/core/controller/MainController.java
@@ -12,6 +12,6 @@ public class MainController {
 
   @GetMapping("/")
   public String mainPage() {
-    return "Hello World! Welc ome test cdcd ";
+    return "Hello World! Welcome test cdcd ";
   }
 }
