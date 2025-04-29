b5756eced72a7790e279a1fe8335f948406f4d78
NamMinwu <minwoo000625@naver.com>
2025-04-29 17:44:10 +0900

    [CHORE] : for check cd4

--- src/main/java/_9oormthonuniv/be/domain/core/controller/MainController.java
+++ src/main/java/_9oormthonuniv/be/domain/core/controller/MainController.java
@@ -12,6 +12,6 @@ public class MainController {
 
   @GetMapping("/")
   public String mainPage() {
-    return "Hello World! Welc. omet est cd d cd ";
+    return "Hello World! Welc. omet escd d cd ";
   }
 }
