af669c5728cf5567955c8fcbbe5899ea833e8066
NamMinwu <minwoo000625@naver.com>
2025-04-30 14:16:37 +0900

    [CHORE] : for check cd plz final plz plz3

--- src/main/java/_9oormthonuniv/be/domain/core/controller/MainController.java
+++ src/main/java/_9oormthonuniv/be/domain/core/controller/MainController.java
@@ -12,6 +12,6 @@ public class MainController {
 
   @GetMapping("/")
   public String mainPage() {
-    return "He ll o W o r ld !  W e lc. o m  e t e s cd d cㄴd ";
+    return "He ll o W o r ld !  W e lcom  e t e s cd d cㄴd ";
   }
 }
