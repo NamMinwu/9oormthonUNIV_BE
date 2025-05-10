10bea498f0c14041616b1bb5f93caac36b4180a9
NamMinwu <minwoo000625@naver.com>
2025-05-10 20:42:02 +0900

    [CHORE] : ci / cd test

--- src/main/java/_9oormthonuniv/be/domain/test/controller/TestController.java
+++ src/main/java/_9oormthonuniv/be/domain/test/controller/TestController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 public class TestController {
 
   // 예시로 나누기 연산을 수행하는 메서드
-  @GetMapping("/divide")
+  @GetMapping("/test")
   public String divide(@RequestParam int dividend, @RequestParam int divisor) throws IOException {
     if (divisor == 0) {
       throw new IOException("이미지가 업로드가 안되었습니다.");
